146310af090306ad6030a919b3405505f2230a42
Achman-awsm <achman234@gmail.com>
2025-08-14 11:09:18 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,7 @@
+print("--WELCOME USER TO TO CHECK THE INPUT IS ODD OR EVEN--")
+n=int(input("Enter Number="))
+if(n%2==0):
+    print("the input number is even")
+else:
+    print("the input number is odd")
+
---
+++ Task2.py
@@ -0,0 +1,4 @@
+s=0
+for i in range(1,51): #the must be execute till 51 because range function one work till end-1
+    s+=i
+print("The sum of all numbers from 1 to 50=",s)
